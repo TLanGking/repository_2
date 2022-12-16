2d3c9fb70e26d7d439a02fadaeb6a9536cdd746d
TLanGking <2753450051@qq.com>
2022-12-16 10:16:58 +0800

    project 4

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace OperatorApplication
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+        public void InitRectangle()
+        {
+            this.length = 3.14;
+            this.width = 6.6;
+        }
+    }
+    class Operator
+    {
+        static void Main(string[] args)
+        {
+            //typeof()
+            Console.WriteLine(typeof(Rectangle));
+            //is
+            Rectangle r;
+            r.InitRectangle();
+            if (r is Rectangle)
+            {
+                Console.WriteLine("r is Rectangle!");
+            }
+        }
+    }
+}
