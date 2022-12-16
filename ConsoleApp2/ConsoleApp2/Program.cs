b4653fcac5cfe4d0ddf214d0f6ca805df5e973f0
TLanGking <2753450051@qq.com>
2022-12-16 09:53:52 +0800

    C# project 2,3

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//C# 基本语法
+using System;
+namespace RectangleAppication
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+        public void Acceptdetials()
+        {
+            length = 3.14;
+            width = 2.71;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("length:{0}", length);
+            Console.WriteLine("width:{0}", width);
+            Console.WriteLine("Area:{0}", GetArea());
+        }
+    }
+
+    class ExcuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptdetials();
+            r.GetArea();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace InputApplication
+{
+    class Input
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            temp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(temp);
+            Console.ReadLine();
+        }
+    }
+}
